67f0b5481c72cf990bea31fac50fed51729a3436
tylerjc5 <tylerjc5@illinois.edu>
2025-05-26 16:34:22 -0500

    Temp

--- apps/settings/src/views/UserManagementNavigation.vue
+++ apps/settings/src/views/UserManagementNavigation.vue
@@ -3,23 +3,25 @@
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <template>
-	<NcAppNavigation class="account-management__navigation"
-		:aria-label="t('settings', 'Account management')">
-		<NcAppNavigationNew button-id="new-user-button"
-			:text="t('settings','New account')"
-			@click="showNewUserMenu"
-			@keyup.enter="showNewUserMenu"
-			@keyup.space="showNewUserMenu">
+	<NcAppNavigation class="account-management__navigation" :aria-label="t('settings', 'Account management')">
+
+		<NcAppNavigationNew button-id="new-account-button" :text="t('settings', 'New account')" @click="showNewUserMenu"
+			@keyup.enter="showNewUserMenu" @keyup.space="showNewUserMenu">
 			<template #icon>
 				<NcIconSvgWrapper :path="mdiPlus" />
 			</template>
 		</NcAppNavigationNew>
 
-		<NcAppNavigationList class="account-management__system-list"
-			data-cy-users-settings-navigation-groups="system">
-			<NcAppNavigationItem id="everyone"
-				:exact="true"
-				:name="t('settings', 'All accounts')"
+		<NcAppNavigationNew button-id="add-existing-account-button" :text="t('settings', 'Add existing account')"
+			@click="showAddExistingUserMenu" @keyup.enter="showAddExistingUserMenu"
+			@keyup.space="showAddExistingUserMenu">
+			<template #icon>
+				<NcIconSvgWrapper :path="mdiAccountPlus" />
+			</template>
+		</NcAppNavigationNew>
+
+		<NcAppNavigationList class="account-management__system-list" data-cy-users-settings-navigation-groups="system">
+			<NcAppNavigationItem id="everyone" :exact="true" :name="t('settings', 'All accounts')"
 				:to="{ name: 'users' }">
 				<template #icon>
 					<NcIconSvgWrapper :path="mdiAccount" />
@@ -31,10 +33,7 @@
 				</template>
 			</NcAppNavigationItem>
 
-			<NcAppNavigationItem v-if="settings.isAdmin"
-				id="admin"
-				:exact="true"
-				:name="t('settings', 'Admins')"
+			<NcAppNavigationItem v-if="settings.isAdmin" id="admin" :exact="true" :name="t('settings', 'Admins')"
 				:to="{ name: 'group', params: { selectedGroup: 'admin' } }">
 				<template #icon>
 					<NcIconSvgWrapper :path="mdiShieldAccount" />
@@ -47,33 +46,36 @@
 				</template>
 			</NcAppNavigationItem>
 
-			<NcAppNavigationItem v-if="isAdminOrDelegatedAdmin"
-				id="recent"
-				:exact="true"
-				:name="t('settings', 'Recently active')"
-				:to="{ name: 'group', params: { selectedGroup: '__nc_internal_recent' } }">
+			<NcAppNavigationItem v-if="isAdminOrDelegatedAdmin" id="recent" :exact="true"
+				:name="t('settings', 'Recently active')" :to="{
+					name: 'group',
+					params: { selectedGroup: '__nc_internal_recent' },
+				}">
 				<template #icon>
 					<NcIconSvgWrapper :path="mdiHistory" />
 				</template>
 				<template #counter>
-					<NcCounterBubble v-if="recentGroup?.usercount"
-						:type="selectedGroupDecoded === '__nc_internal_recent' ? 'highlighted' : undefined">
+					<NcCounterBubble v-if="recentGroup?.usercount" :type="selectedGroupDecoded === '__nc_internal_recent'
+							? 'highlighted'
+							: undefined
+						">
 						{{ recentGroup.usercount }}
 					</NcCounterBubble>
 				</template>
 			</NcAppNavigationItem>
 
 			<!-- Hide the disabled if none, if we don't have the data (-1) show it -->
-			<NcAppNavigationItem v-if="disabledGroup && (disabledGroup.usercount > 0 || disabledGroup.usercount === -1)"
-				id="disabled"
-				:exact="true"
-				:name="t('settings', 'Disabled accounts')"
+			<NcAppNavigationItem v-if="
+				disabledGroup &&
+				(disabledGroup.usercount > 0 || disabledGroup.usercount === -1)
+			" id="disabled" :exact="true" :name="t('settings', 'Disabled accounts')"
 				:to="{ name: 'group', params: { selectedGroup: 'disabled' } }">
 				<template #icon>
 					<NcIconSvgWrapper :path="mdiAccountOff" />
 				</template>
 				<template v-if="disabledGroup.usercount > 0" #counter>
-					<NcCounterBubble :type="selectedGroupDecoded === 'disabled' ? 'highlighted' : undefined">
+					<NcCounterBubble :type="selectedGroupDecoded === 'disabled' ? 'highlighted' : undefined
+						">
 						{{ disabledGroup.usercount }}
 					</NcCounterBubble>
 				</template>
@@ -83,13 +85,11 @@
 		<AppNavigationGroupList />
 
 		<template #footer>
-			<NcButton class="account-management__settings-toggle"
-				type="tertiary"
-				@click="isDialogOpen = true">
+			<NcButton class="account-management__settings-toggle" type="tertiary" @click="isDialogOpen = true">
 				<template #icon>
 					<NcIconSvgWrapper :path="mdiCog" />
 				</template>
-				{{ t('settings', 'Account management settings') }}
+				{{ t("settings", "Account management settings") }}
 			</NcButton>
 			<UserSettingsDialog :open.sync="isDialogOpen" />
 		</template>
@@ -97,55 +97,81 @@
 </template>
 
 <script setup lang="ts">
-import { mdiAccount, mdiAccountOff, mdiCog, mdiPlus, mdiShieldAccount, mdiHistory } from '@mdi/js'
-import { translate as t } from '@nextcloud/l10n'
-import { computed, ref } from 'vue'
+import {
+	mdiAccount,
+	mdiAccountOff,
+	mdiAccountPlus,
+	mdiCog,
+	mdiPlus,
+	mdiShieldAccount,
+	mdiHistory,
+} from "@mdi/js";
+import { translate as t } from "@nextcloud/l10n";
+import { computed, ref } from "vue";
 
-import NcAppNavigation from '@nextcloud/vue/components/NcAppNavigation'
-import NcAppNavigationItem from '@nextcloud/vue/components/NcAppNavigationItem'
-import NcAppNavigationList from '@nextcloud/vue/components/NcAppNavigationList'
+import NcAppNavigation from "@nextcloud/vue/components/NcAppNavigation";
+import NcAppNavigationItem from "@nextcloud/vue/components/NcAppNavigationItem";
+import NcAppNavigationList from "@nextcloud/vue/components/NcAppNavigationList";
+import NcAppNavigationCaption from "@nextcloud/vue/components/NcAppNavigationCaption";
 import NcAppNavigationNew from '@nextcloud/vue/components/NcAppNavigationNew'
-import NcButton from '@nextcloud/vue/components/NcButton'
-import NcCounterBubble from '@nextcloud/vue/components/NcCounterBubble'
-import NcIconSvgWrapper from '@nextcloud/vue/components/NcIconSvgWrapper'
+import NcActionText from "@nextcloud/vue/components/NcActionText";
+import NcButton from "@nextcloud/vue/components/NcButton";
+import NcCounterBubble from "@nextcloud/vue/components/NcCounterBubble";
+import NcIconSvgWrapper from "@nextcloud/vue/components/NcIconSvgWrapper";
 
-import UserSettingsDialog from '../components/Users/UserSettingsDialog.vue'
-import AppNavigationGroupList from '../components/AppNavigationGroupList.vue'
+import UserSettingsDialog from "../components/Users/UserSettingsDialog.vue";
+import AppNavigationGroupList from "../components/AppNavigationGroupList.vue";
 
-import { useStore } from '../store'
-import { useRoute } from 'vue-router/composables'
-import { useFormatGroups } from '../composables/useGroupsNavigation'
+import { useStore } from "../store";
+import { useRoute } from "vue-router/composables";
+import { useFormatGroups } from "../composables/useGroupsNavigation";
 
-const route = useRoute()
-const store = useStore()
+const route = useRoute();
+const store = useStore();
 
 /** State of the 'new-account' dialog */
-const isDialogOpen = ref(false)
+const isDialogOpen = ref(false);
+/** State of the 'add-account' caption */
+const isAddAccountOpen = ref(false);
 
 /** Current active group in the view - this is URL encoded */
-const selectedGroup = computed(() => route.params?.selectedGroup)
+const selectedGroup = computed(() => route.params?.selectedGroup);
 /** Current active group - URL decoded  */
-const selectedGroupDecoded = computed(() => selectedGroup.value ? decodeURIComponent(selectedGroup.value) : null)
+const selectedGroupDecoded = computed(() =>
+	selectedGroup.value ? decodeURIComponent(selectedGroup.value) : null
+);
 
 /** Overall user count */
-const userCount = computed(() => store.getters.getUserCount)
+const userCount = computed(() => store.getters.getUserCount);
 /** All available groups */
-const groups = computed(() => store.getters.getSortedGroups)
-const { adminGroup, recentGroup, disabledGroup } = useFormatGroups(groups)
+const groups = computed(() => store.getters.getSortedGroups);
+const { adminGroup, recentGroup, disabledGroup } = useFormatGroups(groups);
 
 /** Server settings for current user */
-const settings = computed(() => store.getters.getServerData)
+const settings = computed(() => store.getters.getServerData);
 /** True if the current user is a (delegated) admin */
-const isAdminOrDelegatedAdmin = computed(() => settings.value.isAdmin || settings.value.isDelegatedAdmin)
+const isAdminOrDelegatedAdmin = computed(
+	() => settings.value.isAdmin || settings.value.isDelegatedAdmin
+);
 
 /**
  * Open the new-user form dialog
  */
 function showNewUserMenu() {
-	store.commit('setShowConfig', {
-		key: 'showNewUserForm',
+	store.commit("setShowConfig", {
+		key: "showNewUserForm",
 		value: true,
-	})
+	});
+}
+
+/**
+ * Open the add-existing-user dialog
+ */
+function showAddExistingUserMenu() {
+	store.commit("setShowConfig", {
+		key: "showAddExistingUserForm",
+		value: true,
+	});
 }
 </script>
 
@@ -156,6 +182,7 @@ function showNewUserMenu() {
 			will-change: scroll-position;
 		}
 	}
+
 	&__system-list {
 		height: auto !important;
 		overflow: visible !important;
